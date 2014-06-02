76a11c2acd19509e45432dff8d140751e0ff0427
Alejandro Liu <AlejandroL@t-systems.com>
2014-06-02 10:55:23 +0000

    2213

--- refs.sh
+++ refs.sh
@@ -2,15 +2,48 @@
 #
 # Reference functions
 #
+#****f*
+# FUNCTION
+#   create a symbol from a given string
+#
+#   It is meant to sanitize text so it is suitable for variable nameing.
+# SYNOPSIS
+#   mksym txt
+# INPUTS
+#   txt -- text to convert into variable name
+# OUTPUT
+#   sanitized text
+#****
 mksym() {
   tr ' /.a-z-' '___A-Z_'  <<<"$*"| tr -dc '_A-Z0-9'
 }
 
+#****f*
+# FUNCTION
+#   Assigns a value to the named variable.  It is intented to accept
+#   variables for the varname parameter
+# SYNOPSIS
+#   assign varname varvalue
+# INPUTS
+#   varname -- variable to assign a value
+#   value -- value to assign
+#****
 assign() {
   local var="$1"
   eval "$1=\"\$2\""
 }
 
+#****f*
+# FUNCTION
+#   Returns the value of varname.  It is meant to allow variables
+#   to be used for varnames.
+# SYNOPSIS
+#   get varname
+# INPUTS
+#   varname -- variable to lookup
+# OUTPUT
+#   value of varname
+#****
 get() {
   eval echo \"\$$1\"
 }
